616a29dabced8d983f7668687e29ef361934783a
msyaifulbhr <syaifulyy@gmail.com>
2023-10-12 00:18:36 +0700

    add aksesibilitas

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,21 +2,13 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import { displayRestaurants } from '../public/data/data-source.js';
 
-const menu = document.querySelector('#menu');
-const hero = document.querySelector('.hero');
-const main = document.querySelector('main');
-const drawer = document.querySelector('#drawer');
-
-menu.addEventListener('click', function (event) {
-    drawer.classList.toggle('open');
-    event.stopPropagation();
-});
-
-hero.addEventListener('click', function () {
-    drawer.classList.remove('open');
-});
-
-main.addEventListener('click', function () {
-    drawer.classList.remove('open');
-})
+document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.getElementById('menu');
+    const navList = document.getElementById('drawer');
+  
+    menuButton.addEventListener('click', function() {
+      navList.classList.toggle('open');
+      menuButton.classList.toggle('open');
+    });
+  });
 
